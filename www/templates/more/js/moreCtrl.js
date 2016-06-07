bd000eab1e35766ec4a02da48371d5ad4a63319a
Sonam Gupta <sonam.gupta30jan@gmail.com>
2016-06-07 17:53:02 +0530

    back button added

--- www/templates/more/js/moreCtrl.js
+++ www/templates/more/js/moreCtrl.js
@@ -1,16 +1,16 @@
 appControllers.controller('moreCtrl', function($scope,$mdToast,$state) {
 
-  $scope.term_n_cond = function(){
-    $state.go('app.term_n_con');
-  };
+    $scope.term_n_cond = function(){
+     $state.go('app.term_n_con');
+    };
     $scope.refund_policy = function(){
-    $state.go('app.refund_policy');
-  };
+     $state.go('app.refund_policy');
+    };
     $scope.cancellationPolicy = function(){
-    $state.go('app.cancellationPolicy');
-  };
+     $state.go('app.cancellationPolicy');
+    };
     $scope.privacy_policy = function(){
-    $state.go('app.privacy_policy');
-  };
+     $state.go('app.privacy_policy');
+    };
 
 });

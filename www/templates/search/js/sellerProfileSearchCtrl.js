21c5b71261bd40f0460fd72a293312795ce518b5
Sonam Gupta <sonam.gupta30jan@gmail.com>
2016-06-16 12:55:13 +0530

    package to pdp from serch

--- www/templates/search/js/sellerProfileSearchCtrl.js
+++ www/templates/search/js/sellerProfileSearchCtrl.js
@@ -13,5 +13,10 @@ appControllers.controller('sellerProfileSearchCtrl', function ($scope,productSer
     $scope.back_to_search_pdp = function () {
         $state.go('app.search_pdp',{'search_text': $stateParams.search_text,'cat_id': $stateParams.cat_id,'product_id':$stateParams.product_id})
     };
+
+    $scope.getPdpForSearch = function(cat_id,product_id){
+        $state.go('app.search_pdp',{'search_text': $stateParams.search_text,'cat_id':cat_id,'product_id':product_id})
+
+    };
 });
 

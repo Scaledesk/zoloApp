f50c9aef213c4d37b19da8fac2282c9e34281ecb
Sonam Gupta <sonam.gupta30jan@gmail.com>
2016-07-11 13:16:29 +0530

    hardware back button fixed

--- www/js/app.js
+++ www/js/app.js
@@ -763,41 +763,37 @@ angular.module('starter', ['ionic','ngIOS9UIWebViewPatch', 'starter.controllers'
          else if(($state.current.name == "app.search_pdp") &&((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
              $state.go('app.search_info', {'search_text':window.localStorage['search_text']});
          }
-         else if(($state.current.name == "app.search_info") &&((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')) {
+         else if((($state.current.name == "app.search_info") || ($state.current.name == "app.profile") ||
+             ($state.current.name == "app.wishlist") || ($state.current.name == "app.allCategory") ||
+             ($state.current.name == "app.package_list") ||($state.current.name =="app.subCategory") ||
+             ($state.current.name == "app.about_us") ||($state.current.name =="app.more") ||
+             ($state.current.name =="app.contact_us") || ($state.current.name =="app.optional_index"))
+             &&((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')) {
              $ionicHistory.nextViewOptions({
                  disableBack: true
              });
              $state.go('app.home');
          }
-        else if(($state.current.name == "app.product_desc") && ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
+        else if(($state.current.name == "app.product_desc") && ((window.localStorage['access_token'])
+             && (window.localStorage['access_token']) != 'undefined')){
              $state.go('app.package_list', {'sub_cat_id': window.localStorage['sub_cat_id']});
 
          }
-         else if((($state.current.name == "app.package_list") ||($state.current.name =="app.subCategory"))&& ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
-             $ionicHistory.nextViewOptions({
-                 disableBack: true
-             });
-             $state.go('app.home');
-         }
-         else if(($state.current.name == "app.cat_product_desc") && ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
+         else if(($state.current.name == "app.cat_product_desc") &&
+             ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
              $state.go('app.cat_package_list', {'cat_id': window.localStorage['cat_id'],'sub_cat_id':window.localStorage['sub_cat_id']});
          }
-         else if(($state.current.name == "app.cat_package_list") && ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
+         else if(($state.current.name == "app.cat_package_list") &&
+             ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
              $state.go('app.cat_sub_cat_list',{'cat_id':window.localStorage['cat_id']});
          }
-         else if(($state.current.name == "app.cat_sub_cat_list") && ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
+         else if(($state.current.name == "app.cat_sub_cat_list") &&
+             ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
              $ionicHistory.nextViewOptions({
                  disableBack: true
              });
              $state.go('app.allCategory');
          }
-         else if(($state.current.name == "app.allCategory") && ((window.localStorage['access_token']) && (window.localStorage['access_token']) != 'undefined')){
-             $ionicHistory.nextViewOptions({
-                 disableBack: true
-             });
-             $state.go('app.home');
-         }
-
          else {
              navigator.app.backHistory();
          }

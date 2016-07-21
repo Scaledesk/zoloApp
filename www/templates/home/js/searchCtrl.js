3fc71b4ad202f7f831afcf9e94f9b6d0ccb626f2
Sonam Gupta <sonam.gupta30jan@gmail.com>
2016-07-21 11:24:08 +0530

    issue for variable of new searches resolved

--- www/templates/home/js/searchCtrl.js
+++ www/templates/home/js/searchCtrl.js
@@ -1,8 +1,6 @@
 appControllers.controller('searchController', function ($scope, $ionicHistory, $state,$stateParams,$rootScope) {
     $scope.filterText = '';
 
-    var count = 0;
-    
     $scope.text_1 = window.localStorage['text_1'];
     $scope.text_2 = window.localStorage['text_2'];
     $scope.text_3 = window.localStorage['text_3'];
@@ -43,43 +41,41 @@ appControllers.controller('searchController', function ($scope, $ionicHistory, $
         if(text){
             if( window.localStorage['text_1'] == '' || window.localStorage['text_1'] == undefined){
                 window.localStorage['text_1']=text;
-                count = 1;
+                window.localStorage['count'] = 1;
                 console.log("inside if");
             }
            else if(window.localStorage['text_1'] &&
                 ((window.localStorage['text_2'] == '' || window.localStorage['text_2'] == undefined))){
                 window.localStorage['text_2']=text;
-                count = 2;
-
+                window.localStorage['count'] = 2;
                 console.log("inside if 2");
             }
            else if(window.localStorage['text_1'] && window.localStorage['text_2'] &&
                 (window.localStorage['text_3'] == '' || window.localStorage['text_3'] == undefined)){
                 window.localStorage['text_3']=text;
-                count = 3;
+                window.localStorage['count'] = 3;
                 console.log("inside if 3");
            }
-            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && count == 3){
+            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && window.localStorage['count'] == 3){
                 window.localStorage['text_1']=text;
-                count = 2;
+                window.localStorage['count'] = 2;
                 console.log("inside if 4");
             }
-            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && count == 2){
+            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && window.localStorage['count'] == 2){
                 window.localStorage['text_2']=text;
-                count = 1;
+                window.localStorage['count'] = 1;
                 console.log("inside if 5");
             }
-            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && count == 1){
+            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && window.localStorage['count'] == 1){
                 window.localStorage['text_3']=text;
-                count = 0;
+                window.localStorage['count'] = 0;
                 console.log("inside if 6");
             }
-            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && count == 0){
+            else if(window.localStorage['text_1'] && window.localStorage['text_2'] && window.localStorage['text_3'] && window.localStorage['count'] == 0){
                 window.localStorage['text_1']=text;
-                count = 1;
+                window.localStorage['count'] = 1;
                 console.log("inside if 7");
             }
-
             $state.go('app.search_info',{'search_text':text});
         }
     };
